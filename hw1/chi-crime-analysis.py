3c479d07b1a607f3dc8cf8b4849679b392c8cc96
Cecile Murray <29072007+murray-cecile@users.noreply.github.com>
2019-04-07 13:15:06 -0500

    descriptive stats mostly done

--- hw1/chi-crime-analysis.py
+++ hw1/chi-crime-analysis.py
@@ -6,5 +6,24 @@
 #==================================================================#
 
 import pandas as pd
+import geopandas as gpd 
+import censusdata as census
 
-s
+# TO DO
+# pull down some ACS data about Chicago census tracts
+# get shapefiles for Chicago census tracts
+# point in poly merge
+# answer summary stats questions
+
+
+# pull down ACS data on race, income, unemployment, single-parent families, edu attainment
+# tables B02001 (race), B19031 (median household income), B11001 (household type), S1501 (edu)
+
+def get_census_data():
+
+    hhold_vars = map(lambda x: 'B11001_00' + x + 'E', range(1, 10))
+    race_vars = map(lambda x: 'B02001_00' + x + 'E', range(1, 9))
+    all_vars = hhold_vars + race_vars
+
+
+    data = census.download('acs5', 2017, all_vars)
