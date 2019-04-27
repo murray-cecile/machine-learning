116c39df3d3be86715922766d96f78dd47f5bec5
Cecile Murray <29072007+murray-cecile@users.noreply.github.com>
2019-04-27 11:09:22 -0500

    segmenting pipeline into three

---
+++ hw3/pipeline.py
@@ -0,0 +1,141 @@
+#==============================================================================#
+# MACHINE LEARNING FOR PUBLIC POLICY - PIPELINE
+#
+# Cecile Murray
+#==============================================================================#
+
+ # basic dependencies
+import numpy as np
+import pandas as pd 
+import seaborn as sns
+import plotnine as p9
+import matplotlib.pyplot as plt
+
+# my own library of useful functions
+import utils
+
+# classifiers
+import sklearn.tree as tree
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import LinearSVC
+
+# evaluation methods
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score as accuracy
+import graphviz 
+
+
+
+#==============================================================================#
+# BUILD CLASSIFIERS
+#==============================================================================#
+
+def create_train_test_sets(df, target, features, size):
+    ''' wrapper for scikitlearn's train_test_split 
+        Returns: x train, x test, y train, y test'''
+
+    y = df[target]
+    x = df[features]
+
+    return train_test_split(x, y, test_size=size)
+
+
+def build_classifier(classifier_type, x_train, y_train, **params):
+    ''' Takes specified type of classifier using x, y, and optional keyword arguments
+        Returns the trained classifier object
+    '''
+
+    if classifier_type == 'DecisionTree':
+        return DecisionTreeClassifier(params).fit(x_train, y_train)
+
+    elif classifier_type == "LogisticRegression":
+        pass
+    
+    elif classifier_type == "KNN":
+        pass
+    
+    elif classifier_type == "SVM":
+        pass
+
+    else:
+        print("Classifier not supported.")
+        return 
+
+#==============================================================================#
+# EVALUATE CLASSIFIERS
+#==============================================================================#
+
+# This section draws heavily on code developed in ML Lab #2:  
+# https://github.com/dssg/MLforPublicPolicy/blob/master/labs/2019/lab2_sklearn_dt_knn.ipynb
+
+def plot_prediction_distribution(classifier, feature_set):
+    '''Takes decision tree classifier object and associated feature set
+        Returns plot of predicted probability distribution '''
+
+    predicted_scores = classifier.predict_proba(feature_set)[:,1]
+    return plt.hist(predicted_scores)
+
+
+def get_feature_wt(dec_tree, feature_list):
+    ''' returns dict mapping feature names to weights
+    '''
+
+    return dict(zip(feature_list, list(dec_tree.feature_importances_)))
+
+
+def compute_accuracy(classifier, x_data, y_data, threshold):
+    ''' Takes: classifier object, feature and target data, and
+                prediction probability threshold
+        Returns: accuracy of predictions of tree on x for y
+    '''
+
+    pred_scores = classifier.predict_proba(x_data)[:,1]
+    calc_threshold = lambda x,y: 0 if x < y else 1 
+    predicted_test = np.array( [calc_threshold(score, threshold) for score in pred_scores] )
+    return accuracy(predicted_test, y_data)
+
+
+def test_tree_accuracy(x_train, y_train, x_test, y_test, threshold, depths = list(range(0,15, 3)), criterion = ["gini"]):
+    ''' Test different parameters for the tree
+        Returns: data frame summarizing train and test accuracy
+    '''
+
+    results = []
+
+    for c in criterion:
+        for d in depths:
+
+            dec_tree = DecisionTreeClassifier(max_depth=d, criterion=c).fit(x_train, y_train)
+
+            train_acc = compute_accuracy(dec_tree, x_train, y_train, threshold)
+            test_acc = compute_accuracy(dec_tree, x_test, y_test, threshold)
+
+            results.append([c, d, train_acc, test_acc])
+    
+    return pd.DataFrame(results, columns = ['Split Criterion', 'Depth', 'Train Accuracy', 'Test Accuracy'])
+
+
+#==============================================================================#
+# VISUALIZATION TOOLS
+#==============================================================================#
+
+def make_tree_chart(dec_tree, feature_labels, target_names, out_file = ''):
+    ''' Creates a visualization of the tree '''
+
+    if not out_file:
+        out_file = "tree.dot"
+
+    viz = tree.export_graphviz(dec_tree,
+                            feature_names=feature_labels,
+                            out_file= 'figs/' + out_file,
+                            class_names=target_names,
+                            rounded=True,
+                            filled=True)
+
+    with open('figs/' + out_file) as f:
+        dot_graph = f.read()
+        graph = graphviz.Source(dot_graph)
+        
+    return graph
---
+++ hw3/utils.py
@@ -0,0 +1,85 @@
+#==============================================================================#
+# USEFUL FUNCTIONS FOR READING, CLEANING, AND PREPARING DATA
+# CAPP 30254 - MACHINE LEARNING FOR PUBLIC POLICY
+#
+# Cecile Murray
+#==============================================================================#
+
+# dependencies
+import numpy as np
+import pandas as pd 
+import seaborn as sns
+import plotnine as p9
+
+
+#==============================================================================#
+# READ IN DATA
+#==============================================================================#
+
+
+# TO DO: implement additional options
+def read_data(filename, file_type):
+    '''Takes string name of csv or excel file (for now) and returns pandas data frame 
+        If a data dict is provided it will rename vars'''
+    
+    if file_type == "csv":
+        return pd.read_csv('data/' + filename + '.csv')
+    elif file_type == "excel": 
+        return pd.read_excel('data/' + filename + '.xlsx')
+    else:
+        print("filetype not supported")
+        return
+
+
+
+#==============================================================================#
+# PROCESS DATA
+#==============================================================================#
+
+# some pre-processing to convert numeric NA's to the median of the column
+def replace_missing(df, *args, method = 'mean'):
+    ''' Takes data frame, replacement method, and arbitrary column names;
+        replaces missing values using specified method;
+        returns data frame '''
+
+    if method == 'median': 
+        for c in list(args):
+            df[c] = df[c].fillna(df[c].median())
+    
+    elif method == "mean":
+        for c in list(args):
+            df[c] = df[c].fillna(df[c].mean())
+
+    else:
+        pass
+    
+    return df
+
+#==============================================================================#
+# GENERATE FEATURES
+#==============================================================================#
+
+def bin_continuous(df, var, new_varname, breaks, labels = False):
+    ''' Convert continuous variable to discrete/categorical '''
+
+
+    # handle case where upper bound less than max of variable
+    if breaks[-1] < df[var].max():
+        breaks.append(df[var].max())
+        
+    # handle case where lower bound greater than min of variable
+    if breaks[0] > df[var].min():
+        breaks.insert(0, df[var].min())
+
+    if labels:
+        df[new_varname] = pd.cut(df[var], np.array(breaks), labels = labels)
+    else:
+        df[new_varname] = pd.cut(df[var], np.array(breaks))
+    
+    return df
+
+
+def make_cat_dummy(df, cols):
+    '''Convert categorical variable or list of categorical variables into binary '''
+
+    return pd.get_dummies(df, dummy_na=True, columns=cols)
