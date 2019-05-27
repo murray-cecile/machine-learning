may have fixed leakage?

hw3/build_models.py
@@ -20,7 +20,21 @@ import exploration as exp
 import pipeline as pipe
 
 
+def convert_dates(df):
+    ''' Convert date fields to datetime and compute target variable:
+        project fails to be funded within 60 days of being posted on DonorsChoose
+    '''
+
+    # convert date fields and compute whether project is funded within 60 days
+    df['date_posted'] = pd.to_datetime(df.date_posted) 
+    df['datefullyfunded'] = pd.to_datetime(df.datefullyfunded)
+    df['not_funded'] = np.where(df.datefullyfunded - df.date_posted > datetime.timedelta(days=60), 1, 0)
+
+    return df
+
+
 def prepare_data(df):
+    ''' Perform a range of application-specific data cleaning tasks '''
 
         # convert categorical variables to binary
     categorical_list = ['school_metro',
@@ -47,17 +61,17 @@ def prepare_data(df):
                                         'school_magnet',
                                         'eligible_double_your_impact_match'], 't', 'f')
 
-    # replace NAs in students reached
-    proj['students_reached'] = proj['students_reached'].fillna(0)
+    return proj
 
-    # normalize project price and students reached
-    proj['total_price_norm'] = preprocessing.scale(proj['total_price_including_optional_support'].astype('float64'))
-    proj['students_reached_norm'] = preprocessing.scale(proj['students_reached'].astype('float64'))
 
-    # convert date fields and compute whether project is funded within 60 days
-    proj['date_posted'] = pd.to_datetime(proj.date_posted) 
-    proj['datefullyfunded'] = pd.to_datetime(proj.datefullyfunded)
-    proj['not_funded'] = np.where(proj.datefullyfunded - proj.date_posted > datetime.timedelta(days=60), 1, 0)
+def normalizer_func(df):
+    ''' Application-specific function to replace NA's in students reached and
+        to normalize project price and students reached
+    '''
+
+    df['students_reached'] = df['students_reached'].fillna(0)
+    df['total_price_norm'] = preprocessing.scale(df['total_price_including_optional_support'].astype('float64'))
+    df['students_reached_norm'] = preprocessing.scale(df['students_reached'].astype('float64'))
 
     return df
 
@@ -67,8 +81,8 @@ if __name__ == "__main__":
     # read data in    
     projraw = utils.read_data('projects_2012_2013', 'csv')
 
-    # clean the data
-    proj = prepare_data(projraw)
+    # convert date fields and label target variable
+    proj = convert_dates(projraw)
 
     # select features by set difference
     not_feature_cols = ['teacher_acctid',
@@ -91,9 +105,33 @@ if __name__ == "__main__":
     features = list(set(proj.columns).difference(not_feature_cols))
 
     # create training and testing sets: expanding window cross-validation
-    lag_time = datetime.timedelta(days=60)
-    proj = pipe.create_sliding_window_sets(proj, 'date_posted', features, 'not_funded', 26)
+    proj = pipe.create_sliding_window_sets(projraw,
+                                            'date_posted',
+                                            features,
+                                            'not_funded',
+                                            26)
+
+    # clean the data (operations affect only each row in isolation)
+    proj = prepare_data(projraw)
+
+    # loop over intervals and perform normalizations
+    for i in pipe.get_date_intervals(proj, 'interval'):
+        proj = utils.transform_vars_safely(proj,
+                                            normalizer_func,
+                                            'date_posted',
+                                            'projectid',
+                                            i.left,
+                                            i.right)
+
+    # proj.drop(columns = not_feature_cols)
+    features = list(set(proj.columns).difference(not_feature_cols))
 
     # run all 165 models across 7 thresholds
-    pipe.test_over_time(proj, features, 'not_funded', 'interval', 'date_posted', lag_time=lag_time,
-                    to_file = 'full_run.csv', classifier_dict = pipe.CLASSIFIERS)
+    pipe.test_over_time(proj,
+                        features,
+                        'not_funded', 
+                        'interval', 
+                        'date_posted', 
+                        lag_time=datetime.timedelta(days=60),
+                        to_file = 'revised_test_run.csv',
+                        classifier_dict = pipe.TEST_CLASSIFIERS)

hw3/utils.py
@@ -29,8 +29,6 @@ def read_data(filename, file_type):
         print("filetype not supported")
         return
 
-
-
 #==============================================================================#
 # PROCESS DATA
 #==============================================================================#
@@ -79,6 +77,23 @@ def convert_to_boolean(df, cols, true_val, false_val):
         df[col] = np.where(df[col] == true_val, True, False)
     
     return df
+
+
+def transform_vars_safely(df, func, date_col, id_col, start_date, end_date, *func_args):
+    '''Takes: data frame, location of date and unique ID column,
+              variable transformation function, and start/end dates
+
+       Returns: data frame where observations within specified dates have been
+        transformed by provided function, only with regard to other observations
+        within those dates (so variables are transformed without leakage)
+    '''
+
+    # take subset of main data frame within specified dates and apply func
+    subdf = df.loc[(df[date_col] >= start_date) & (df[date_col] <= end_date)]
+    subdf = func(subdf, *func_args)
+
+    # then replace the rows in the main dataframe with the transformed rows
+    return pd.concat([df[~df['projectid'].isin(subdf['projectid'])], subdf])
 
 #==============================================================================#
 # GENERATE FEATURES